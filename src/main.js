e9868dc56c4ff415df87b57f1c5bf24fdcb1dcac
xstxhjh <573363596@qq.com>
2020-09-11 11:07:30 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
-import WebComponents from './components/WebComponents/test.js'
+import './components/WebComponents/test.js'
 
 createApp(App).mount('#app')

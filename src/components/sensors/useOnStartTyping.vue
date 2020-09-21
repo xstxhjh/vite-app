43087b196a949842a2e1c50bd13ef32f4ab6f0b0
xstxhjh <573363596@qq.com>
2020-09-21 09:55:07 +0800

    use doc

--- src/components/sensors/useOnStartTyping.vue
+++ src/components/sensors/useOnStartTyping.vue
@@ -14,7 +14,9 @@ export default {
   setup() {
     const input = ref(null)
 
+    // 当用户鼠标在页面上没有聚焦的时候，键盘键入任意即可聚焦
     onStartTyping(() => {
+      console.log(input)
       if (!input.value.active) input.value.focus()
     })
 

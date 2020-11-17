2a195250a65766b09e9ede910bc95be1c9ed37a9
xstxhjh <573363596@qq.com>
2020-11-17 14:52:35 +0800

    css变量提案修改

--- src/components/RFC/style-vars/index.vue
+++ src/components/RFC/style-vars/index.vue
@@ -9,6 +9,9 @@ export default {
   setup() {
     const state = reactive({
       color: 'red',
+      font: {
+        size: '2em',
+      },
     })
 
     return { ...toRefs(state) }
@@ -20,4 +23,12 @@ export default {
 .text {
   color: var(--color);
 }
+/*
+  .text {
+    color: var(--v-bind:color);
+
+    shorthand + nested property access
+    font-size: var(--:font.size);
+  } 
+*/
 </style>

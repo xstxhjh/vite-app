9eef044fb10cc4c105400cb9553571646544c185
xstxhjh <573363596@qq.com>
2020-11-17 17:09:33 +0800

    css var

--- src/components/RFC/style-vars/index.vue
+++ src/components/RFC/style-vars/index.vue
@@ -3,18 +3,19 @@
 </template>
 
 <script>
-import { reactive, toRefs, getCurrentInstance } from 'vue'
+import { reactive, toRefs, useCssVars } from 'vue'
 
 export default {
   setup() {
-    const { ctx } = getCurrentInstance()
-    
     const state = reactive({
-      color: ctx.color,
       font: {
         size: '2em',
-      },
+      }
     })
+
+    useCssVars((_ctx) => ({
+      color: _ctx.color
+    }))
 
     return { ...toRefs(state) }
   },
@@ -22,15 +23,20 @@ export default {
 </script>
 
 <style vars="{ color }">
+:root {
+  --fontSize: 22px;
+}
+
 .text {
   color: var(--color);
+  font-size: var(--fontSize);
 }
-/*
-  .text {
+
+  /* .text {
     color: var(--v-bind:color);
 
     shorthand + nested property access
-    font-size: var(--:font.size);
-  } 
-*/
+    font-size: var(--font.size);
+  }  */
+
 </style>
